497bf19b0f5fc6bf0b34f6523749814bc4338402
Gustavo Martins <gustavo.mtborges@gmail.com>
2014-07-03 22:58:15 -0300

    Problema 1001 URI - Extremely Basic

---
+++ src/Sum.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * Created by gustavo on 7/3/14.
+ */
+ class Sum {
+
+
+    public static int sumNumbers(int a, int b) {
+
+        int result;
+
+        result = a + b;
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int result = a + b;
+
+        System.out.println("X = " + result);
+
+        sc.close();
+    }
+}
---
+++ test/SumTest.java
@@ -0,0 +1,16 @@
+
+import org.junit.Test;
+
+/**
+ * Created by gustavo on 7/3/14.
+ */
+public class SumTest {
+
+    @Test
+    public void testSum(){
+
+        Sum sum = new Sum();
+        sum.sumNumbers(10, 9);
+
+    }
+}
